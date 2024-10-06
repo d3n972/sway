bf3f5dc7800bbc814b215f167facafaaed6f4dab
d3n <d3n@d3n.it>
2024-10-06 10:24:39 +0200

    add swaylock hotkey and background

--- startup.sh
+++ startup.sh
@@ -1,7 +1,14 @@
-#!/bin/bash
+#!/bin/zsh
 
 swaymsg output "DP-1" disable
 swaymsg output "eDP-1" disable
 swaymsg output "eDP-1" enable
 swaymsg output eDP-1 pos 0 0
 swaymsg input type:keyboard xkb_layout hu
+if [[ ! $(pgrep -f 1password) ]]; then
+    nohup 1password --silent > /dev/null &
+fi
+
+if [[ ! $(pgrep -f Discord) ]]; then
+    Discord
+fiQ
