807f4aa28a31a459c5439344b1e184c0e96e392f
d3n <d3n@d3n.it>
2024-10-13 13:02:36 +0200

    refactor

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-for script in $(ls ~/.config/sway/scripts | sort -g); do
+for script in $(ls ~/.config/sway/startup-scripts | sort -g); do
     echo "Running $script"
     bash ~/.config/sway/scripts/$script
 done
